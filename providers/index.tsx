067501c36486ab1491c8fa1464270cc34ca3f3a7
GDD-impact <gddimpactsoftwares@gmail.com>
2025-01-06 09:10:57 +0100

    updates

---
+++ providers/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import QueryProvider from "./react-query";
+
+const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <QueryProvider>{children}</QueryProvider>;
+};
+
+export default Providers;
